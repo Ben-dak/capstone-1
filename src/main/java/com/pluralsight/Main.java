65ec30aa2eee00d6783a95f6122e4bf6ed7ab2a4
Dominick <dominickfrench.oe@gmail.com>
2025-10-10 13:07:42 -0500

    Added while loop and else if for the home screen.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("""
+                D) Add Deposit
+                P) Make Payment (Debit)
+                L) Ledger
+                X) Exit
+                """);
+
+            System.out.println("Select one of the options above: ");
+            String option = myScanner.nextLine();
+
+            if (Objects.equals(option, "D")) {
+                System.out.println("Enter the amount you would like to deposit: ");
+                //Need to prompt user for the deposit information and save it to the csv file
+            } else if (option == "P") {
+                System.out.println("Enter your debit card information: ");
+            } else if (option == "L") {
+                System.out.println("Here is the Ledger: ");
+            }else{
+                System.out.println("----Application Closed----");
+                break;
+            }
+        }
+    }
+
 }
