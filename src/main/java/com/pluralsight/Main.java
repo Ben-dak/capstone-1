b2c4c415cb61578c17c2c9663c04b57ec914dff7
Dominick <dominickfrench.oe@gmail.com>
2025-10-12 11:43:43 -0500

    Replaced "while" with "do while", added "Objects.equals", and added appropriate breaks.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -7,33 +7,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        while (true) {
+        do {
             System.out.println("""
-                D) Add Deposit
-                P) Make Payment (Debit)
-                L) Ledger
-                X) Exit
-                """);
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Ledger
+                    X) Exit
+                    """);
 
             System.out.println("Select one of the options above: ");
             String option = myScanner.nextLine();
 
             if (Objects.equals(option, "D")) {
                 System.out.println("Enter the amount you would like to deposit: ");
+                break;
                 //Need to prompt user for the deposit information and save it to the csv file
-            } else if (option == "P") {
+            } else if (Objects.equals(option, "P")) {
                 System.out.println("Enter your debit card information: ");
+                break;
                 //Need to prompt user for the debit information and save it to the csv file
-            } else if (option == "L") {
+            } else if (Objects.equals(option, "L")) {
                 System.out.println("Here is the Ledger: ");
+                break;
                 //Need to display the ledger screen
-            }else{
+            } else {
                 System.out.println("----Application Closed----");
                 break;
 
 
             }
-        }
+        } while (true);
     }
 
 }
