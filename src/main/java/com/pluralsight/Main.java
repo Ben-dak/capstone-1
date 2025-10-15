b77d1f195b6d4474333527892d2c3dce25ef470e
Dominick <dominickfrench.oe@gmail.com>
2025-10-15 10:16:20 -0500

    Cleaned up and shortened code in the Main class by adding a switch statement for mainMenu.  Now working on ledgerMenu.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,42 +1,45 @@
 package com.pluralsight;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String fileName = "transactions.csv";
+        mainMenu();
+    }
+
+    private static void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        char userInput;
         do {
             System.out.println("""
+                    === Home Menu ===
                     D) Add Deposit
                     P) Make Payment (Debit)
                     L) Ledger
                     X) Exit
                     """);
+            System.out.print("Choose an option: ");
+            userInput = scanner.next().toUpperCase().charAt(0);
 
-            System.out.println("Select one of the options above: ");
-            String option = myScanner.nextLine();
-
-            if (Objects.equals(option, "D")) {
-                System.out.println("Enter the amount you would like to deposit: ");
-                break;
-                //Need to prompt user for the deposit information and save it to the csv file
-            } else if (Objects.equals(option, "P")) {
-                System.out.println("Enter your debit card information: ");
-                break;
-                //Need to prompt user for the debit information and save it to the csv file
-            } else if (Objects.equals(option, "L")) {
-                System.out.println("Here is the Ledger: ");
-                break;
-                //Need to display the ledger screen
-            } else {
-                System.out.println("----Application Closed----");
-                break;
-
-
+            switch (userInput) {
+                case 'D' -> System.out.println("Add Deposit selected...");
+                case 'P' -> System.out.println("Make Payment selected...");
+                case 'L' -> ledgerMenu();
+                case 'X' -> System.out.println("Exiting application...");
+                default -> System.out.println("Invalid option. Please try again.");
             }
-        } while (true);
+        } while (userInput != 'X');
     }
 
+    private static void ledgerMenu() {
+        Scanner scanner = new Scanner(System.in);
+        char userInput;
+
+        do {
+
+        }while(true);
+
+    }
 }
