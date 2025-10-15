65bd2312839953fe757fdc95a235241bd7002d76
Ben-dak <159968206+Ben-dak@users.noreply.github.com>
2025-10-15 10:27:26 -0500

    Update Main.java
    Cleaned up and shortened code in the Main class by adding a switch statement for mainMenu.  Now working on ledgerMenu.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,15 +3,43 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
+        String fileName = "transactions.csv";
+        mainMenu();
+    }
 
-        ConsoleInterface io = new ConsoleInterface();
-        CsvRepo repo = new CsvRepo();
-        TransactionService svc = new TransactionService();
-        ReportService reports = new ReportService();
-        LedgerView ledger = new LedgerView();
+    private static void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        char userInput;
+        do {
+            System.out.println("""
+                    === Home Menu ===
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Ledger
+                    X) Exit
+                    """);
+            System.out.print("Choose an option: ");
+            userInput = scanner.next().toUpperCase().charAt(0);
 
+            switch (userInput) {
+                case 'D' -> System.out.println("Add Deposit selected...");
+                case 'P' -> System.out.println("Make Payment selected...");
+                case 'L' -> ledgerMenu();
+                case 'X' -> System.out.println("Exiting application...");
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+        } while (userInput != 'X');
+    }
+
+    private static void ledgerMenu() {
+        Scanner scanner = new Scanner(System.in);
+        char userInput;
+
+        do {
+
+        }while(true);
 
     }
 }
