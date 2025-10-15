9ae770fb4aae66160dbb7ed2e41225d1a4e1e5f8
Dominick <dominickfrench.oe@gmail.com>
2025-10-15 16:32:43 -0500

    Added getters, setters, and constructors.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,16 +1,32 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+    String name;
+
     public static void main(String[] args) {
         String fileName = "transactions.csv";
-        mainMenu();
+        // mainMenu();
+
+//        Transaction amazon;
+//        amazon = new Transaction("Amazon");
+
+        LocalDate today = LocalDate.now();
+        String vendor = "Amazon";
+        LocalTime clock = LocalTime.now();
+        String desc = "Keyboard";
+        double price = 89.50;
+        Transaction transactions = new Transaction(today, vendor, clock, desc, price);
+
     }
 
     private static void mainMenu() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
         char userInput;
         do {
             System.out.println("""
@@ -21,7 +37,7 @@ public class Main {
                     X) Exit
                     """);
             System.out.print("Choose an option: ");
-            userInput = scanner.next().toUpperCase().charAt(0);
+            userInput = myScanner.next().toUpperCase().charAt(0);
 
             switch (userInput) {
                 case 'D' -> System.out.println("Add Deposit selected...");
@@ -35,11 +51,31 @@ public class Main {
 
     private static void ledgerMenu() {
         Scanner scanner = new Scanner(System.in);
-        char userInput;
-
+        String userInput;
         do {
+            System.out.println("""
+                    === Home Menu ===
+                    A) Print all transactions
+                    H) Exit
+                    """);
+            System.out.print("Choose an option: ");
+            userInput = scanner.next();
 
-        }while(true);
+            switch (userInput) {
+                case "A":
+                    printAllTransactions();
+                    break;
+                case "H":
+                    System.out.println("Exiting application...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (userInput.equals("H"));
+    }
+
+    static void printAllTransactions() {
 
     }
+
 }
